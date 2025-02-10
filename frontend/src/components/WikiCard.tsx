6f53dae1be367ae416f24560f0c78824b1df7ddf
Matt Harcourt <matt.s.harcourt@gmail.com>
2025-02-10 08:46:05 -0800

    For large screens: display image at original aspect ratio over an opaque version of the image expanded to fill the display.

--- frontend/src/components/WikiCard.tsx
+++ frontend/src/components/WikiCard.tsx
@@ -50,14 +50,18 @@ export function WikiCard({ article }: WikiCardProps) {
 
     return (
         <div className="h-screen w-full flex items-center justify-center snap-start relative" onDoubleClick={() => toggleLike(article)}>
-            <div className="h-full w-full relative">
+            <div className="h-full w-full relative bg-white">
                 {article.thumbnail ? (
-                    <div className="absolute inset-0">
+                    <div
+                        style={{ '--bg-image': `url('${article.thumbnail.source}')` }}
+                        className={`absolute inset-0 lg:bg-cover lg:bg-[image:var(--bg-image)]`}
+                    >
+                        <div className="absolute inset-0 lg:bg-black lg:opacity-50"/>
                         <img
                             loading="lazy"
                             src={article.thumbnail.source}
                             alt={article.displaytitle}
-                            className={`w-full h-full object-cover transition-opacity duration-300 bg-white ${imageLoaded ? 'opacity-100' : 'opacity-0'
+                            className={`w-full h-full lg:relative lg:inset-1/20 lg:w-9/10 lg:h-9/10 object-cover lg:object-contain transition-opacity duration-300 bg-white lg:bg-transparent ${imageLoaded ? 'opacity-100' : 'opacity-0'
                                 }`}
                             onLoad={() => setImageLoaded(true)}
                             onError={(e) => {
